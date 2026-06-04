a700853c5415b3b58c73ed87ced770b32c90053a
NHT2-gh <thanhnn489@gmail.com>
2026-06-04 15:16:35 +0700

    fix: correct TextArea component import path casing

--- src/components/_cms/ui/input/index.ts
+++ src/components/_cms/ui/input/index.ts
@@ -1,7 +1,7 @@
 export { default as Radio } from "./Radio";
 export { default as Input } from "./Input";
 export { default as Checkbox } from "./Checkbox";
-export { default as TextArea } from "./Textarea";
+export { default as TextArea } from "./TextArea";
 export { default as FileInput } from "./FileInput";
 export { default as CopyInput } from "./CopyInput";
 export { default as PhoneInput } from "./PhoneInput";
